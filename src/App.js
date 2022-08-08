82e0611f4d9e0233281934e04512a69de1e023ba
awais004800 <amaaz0434@gmail.com>
2022-08-08 17:43:22 +0500

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Resturant from './Component/Resturant';
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyName/>
+      <Resturant/>
+   <h1 className="myclasname"> </h1>
+   <p></p>
+     
     </div>
   );
-}
+  }
+  const MyName=()=>{
+
+return <h1></h1>
+  }
+
 
 export default App;
